e20bce2dc881266b214ac36cbb6eee022a421411
Marco Anotnio Nunez Perez <mnunez@nearsoft.com>
2016-10-02 18:43:26 -0500

    Pass first 3 tests

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -1,7 +1,9 @@
 package nearsoft.academy.bigdata.recommendation;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
+import java.io.*;
+import java.util.*;
+import java.util.zip.GZIPInputStream;
+
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
@@ -13,181 +15,222 @@ import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.zip.GZIPInputStream;
+public class MovieRecommender{
 
-public class MovieRecommender {
+    private String filepath;
+    private HashMap usersMap = new HashMap();
+    private HashMap productsMap = new HashMap();
+    private int reviews = 0;
+    private final String FILEPATH_TO_WRITE = "/home/marco/Documents/Nearsoft/Weeks/03/real/big-data-exercises/MRFormat.csv";
 
-    private final String CSV_FILENAME = "MoviesFormated.csv";
-    private final int NUMBER_RECOMMENDATIONS = 3;
-    private final String[] REVIEW_INFORMATION = {"review/userId", "product/productId", "review/score"};
-    private final String CHARSET_DECODER = "UTF8";
-    private UserBasedRecommender recommender;
-    private int totalReviews = 0;
-    private HashBiMap<String, Integer> usersList;
-    private HashBiMap<String, Integer> productsList;
+    public MovieRecommender(String filepath){
 
-    public MovieRecommender(String dataSource){
-
-        try{
-            this.setupAttrs(dataSource);
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+        this.filepath = filepath;
+        this.createCSV(this.filepath);
 
     }
 
-    private final void setupAttrs(String dataSource){
+    public int getTotalReviews(){
 
-        try{
-            String csvPathFile = FileToCSVFormatFile(dataSource);
-            DataModel model = new FileDataModel(new File(csvPathFile));
-            UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-            UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
-            recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+        return this.reviews;
 
-    }
-
-    /*
-        Read the movies data file, create the csv file required, and update: totalReviews, productsList, usersList
-    */
-    private String FileToCSVFormatFile(String dataSource){
-
-        Writer writer = null;
-        this.usersList = HashBiMap.create();
-        this.productsList = HashBiMap.create();
-
-        try{
-
-            File source = new File(dataSource);
-            File csvFile = new File(source.getParentFile().getAbsolutePath() + "/" + CSV_FILENAME);
-            InputStream fileStream = new FileInputStream(dataSource);
-            InputStream gzipStream = new GZIPInputStream(fileStream);
-            Reader decoder = new InputStreamReader(gzipStream, CHARSET_DECODER);
-            BufferedReader buffered = new BufferedReader(decoder);
-
-            if(csvFile.exists()){
-                csvFile.delete();
-            }
-
-            csvFile.createNewFile();
-
-            writer = new BufferedWriter(new FileWriter(csvFile));
-            String currentFileLine;
-            Integer userId = null;
-            String score = "";
-            Integer productId = null;
-
-            while((currentFileLine = buffered.readLine()) != null){
-
-                /* review/userId */
-                if(currentFileLine.contains(REVIEW_INFORMATION[0])){
-
-                    this.totalReviews++;
-
-                    String userName = extractValue(currentFileLine);
-
-                    if(!this.usersList.containsKey(userName)){
-
-                        this.usersList.put(userName, this.usersList.size() + 1);
-
-                    }
-
-                    userId = this.usersList.get(userName);
-
-                }
-                /* product/productId */
-                else if(currentFileLine.contains(REVIEW_INFORMATION[1])){
-
-                    String productName = extractValue(currentFileLine);
-
-                    if(!this.productsList.containsKey(productName)){
-
-                        this.productsList.put(productName, this.productsList.size() + 1);
-
-                    }
-
-                    productId = this.productsList.get(productName);
-
-                }
-                /* review/score */
-                else if(currentFileLine.contains(REVIEW_INFORMATION[2])){
-
-                    score = extractValue(currentFileLine);
-
-                    writer.append(String.valueOf(userId) + ',' + String.valueOf(productId) + ',' + score + "\n");
-
-                }
-
-            }
-
-        return csvFile.getAbsolutePath();
-
-        }catch(IOException e) {
-
-            e.printStackTrace();
-            throw new RuntimeException("Error processing the data source " + dataSource, e);
-
-        }
-        finally{
-            try{
-                if(writer != null){
-                    writer.close();
-                }
-            }
-            catch(Exception e){
-                e.printStackTrace();
-            }
-        }
     }
 
     public int getTotalProducts(){
 
-        return this.productsList.size();
+        return this.getProductsMap().size();
 
     }
 
     public int getTotalUsers(){
 
-        return this.usersList.size();
+        return this.getUsersMap().size();
 
     }
 
-    public int getTotalReviews() {
 
-        return this.totalReviews;
 
+    public String getFilepath(){
+        return this.filepath;
     }
 
-    public List<String> getRecommendationsForUser(String userName) throws TasteException{
+    public HashMap getUsersMap(){
+        return this.usersMap;
+    }
 
-        List<RecommendedItem> recommendations = recommender.recommend(this.usersList.get(userName), NUMBER_RECOMMENDATIONS);
-        List<String> recommendedMovies = new ArrayList<String>();
-        BiMap<Integer, String> productsByName = this.productsList.inverse();
+    public HashMap getProductsMap(){
+        return this.productsMap;
+    }
 
-        for(RecommendedItem recommendation : recommendations){
+    public void setUsersMap(String key, String value){
+        this.usersMap.put(key, value);
+    }
 
-            String movieName = productsByName.get((int)recommendation.getItemID());
-            recommendedMovies.add(movieName);
+    public void setProductsMap(String key, String value){
+        this.productsMap.put(key, value);
+    }
+
+    public int getReviews(){
+        return this.reviews;
+    }
+
+    public void incrementReviews(){
+        this.reviews++;
+    }
+
+
+    private void createCSV(String filepath){
+
+        String currentLine = null;
+        BufferedWriter bufferedWriter = null;
+
+        try{
+
+            InputStream fileStream = new FileInputStream(filepath);
+            InputStream gzipStream = new GZIPInputStream(fileStream);
+            Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
+            BufferedReader buffered = new BufferedReader(decoder);
+
+            File file = new File(FILEPATH_TO_WRITE);
+
+            if(!file.exists()){
+
+                file.createNewFile();
+
+            }
+
+            FileWriter fileWriter = new FileWriter(file);
+            bufferedWriter = new BufferedWriter(fileWriter);
+
+            String productID = null;
+            String userID = null;
+            String score = null;
+
+            while( (currentLine = buffered.readLine()) != null ){
+
+                if(currentLine.contains("review/text:")){ continue; }
+
+                String reviewInfo = null;
+
+                if(currentLine.contains("product/productId:")){
+
+                    this.incrementReviews();
+
+                    reviewInfo = currentLine.split("product/productId:")[1].trim();
+                    HashMap mp = this.getProductsMap();
+
+                    if(mp.get(reviewInfo) == null){
+
+                        this.setProductsMap(reviewInfo, Integer.toString(mp.size() + 1));
+
+                    }
+
+                    productID = (String)mp.get(reviewInfo);
+
+                }
+                else if(currentLine.contains("review/userId:")){
+
+                    reviewInfo = currentLine.split("review/userId:")[1].trim();
+                    HashMap mp = this.getUsersMap();
+
+                    if(mp.get(reviewInfo) == null){
+
+                        this.setUsersMap(reviewInfo, Integer.toString(mp.size() + 1));
+
+                    }
+                    userID = (String)mp.get(reviewInfo);
+
+                }
+                else if(currentLine.contains("review/score:")){
+
+                    score = currentLine.split("review/score:")[1].trim();
+
+                    bufferedWriter.write(userID + "," + productID + "," + score + "\n");
+                }
+
+
+            }
+
+            System.out.println("REVIEWS: " + this.getTotalReviews());
+            System.out.println("PRODUCTS: " + this.getTotalProducts());
+            System.out.println("USERS: " + this.getTotalUsers());
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+
+            try{
+                if(bufferedWriter != null){
+                    bufferedWriter.close();
+                }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
 
         }
 
-        return recommendedMovies;
     }
 
-    private String extractValue(String str){
+    private void processCurrentLine(String currentLine){
 
-        return str.substring(str.indexOf(":") + 2, str.length());
+        String reviewInfo = null;
+
+        if(currentLine.contains("product/productId:")){
+
+            this.incrementReviews();
+
+            // System.out.println("PRODUCT ID");
+            reviewInfo = currentLine.split("product/productId:")[1].trim();
+            HashMap mp = this.getProductsMap();
+
+            if(mp.get(reviewInfo) == null){
+
+                this.setProductsMap(reviewInfo, Integer.toString(mp.size() + 1));
+
+            }
+
+        }
+        else if(currentLine.contains("review/userId:")){
+
+            reviewInfo = currentLine.split("review/userId:")[1].trim();
+            HashMap mp = this.getUsersMap();
+
+            if(mp.get(reviewInfo) == null){
+
+                this.setUsersMap(reviewInfo, Integer.toString(mp.size() + 1));
+
+            }
+
+        }
 
     }
+
+    private void listHashMap(HashMap hashmap){
+
+        Set set = hashmap.entrySet();
+        Iterator i = set.iterator();
+
+
+        while(i.hasNext()){
+
+            Map.Entry entry = (Map.Entry)i.next();
+            // System.out.println("Key: " + entry.getKey());
+            // System.out.println("Value: " + entry.getValue());
+
+        }
+        System.out.println("Size: " + hashmap.size());
+
+    }
+
+    // public static void main(String[] args) {
+    //
+    //     // MovieRecommender mv = new MovieRecommender("./smallMovies.txt.tar.gz");
+    //     MovieRecommender mv = new MovieRecommender("./movies.txt.gz");
+    //     // System.out.println("File path: " + mv.getFilepath());
+    //
+    // }
 
 }
